583455e08f48073f222aac6946ef85883d0ade66
Lance Pitka <lance@tealev.com>
2026-06-08 22:35:07 -0600

    Basic app layout

---
+++ firmware/Core/Src/app/app.c
@@ -0,0 +1,15 @@
+/**
+ * @file app.c
+ * @brief Application code for Pressure PID Controller
+ */
+
+#include "app.h"
+
+void app(void)
+{
+    /* Application code goes here */
+    while (1)
+    {
+        // Main loop
+    }
+}
---
+++ firmware/Core/Src/app/app.h
@@ -0,0 +1,12 @@
+/**
+ * @file    app.h
+ * @brief   Header file for app.c
+ */
+
+#ifndef APP_H_
+#define APP_H_
+
+/** Application entry point. Forever loop */
+void app(void);
+
+#endif /* APP_H_ */
