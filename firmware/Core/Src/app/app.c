f71d649c797e65300c0a5dfba62e906992e9569f
Lance Pitka <lance@tealev.com>
2026-06-08 22:45:41 -0600

    WIP

--- firmware/Core/Src/app/app.c
+++ firmware/Core/Src/app/app.c
@@ -4,9 +4,18 @@
  */
 
 #include "app.h"
+#include "util/logging.h"
 
 void app(void)
 {
+    /* Set log level */
+    Log_SetLevel(LOG_LEVEL_INFO);
+
+    /* Print startup message */
+    printf("=============================================\r\n");
+    printf("      Espresso Pressure Controller \r\n");
+    printf("=============================================\r\n\r\n");
+
     /* Application code goes here */
     while (1)
     {
